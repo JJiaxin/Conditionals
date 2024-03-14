fda39a464bde3dcf613fc29980b7f233acb5fa38
JJiaxin <86167540+JJiaxin@users.noreply.github.com>
2024-03-14 23:15:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function getInputValue(){
+    var e = document.getElementById("mySelect").value;
+    document.getElementById("display").innerHTML= "<br>" + e;
+
+var year = e;
+
+if (year === '1993') {
+document.getElementById('anime').innerHTML= "<img src='Slam_Dunk.jpg'>";
+}
+else if(year === '1999'){
+document.getElementById("anime").innerHTML= "<img src='digimon.jpg' height=400px width=600px>";
+}
+else if (year === '2000'){
+document.getElementById("anime").innerHTML= "<img src='digi02.jpg'>";
+  
+ } 
+else if(year === '2001'){
+document.getElementById("anime").innerHTML= "<img src='shamanking.jpg'>";
+} 
+else if(year === '2002'){
+document.getElementById("anime").innerHTML= "<img src='naruto.jpg'>";
+ } 
+else if(year === '2004'){
+    document.getElementById("anime").innerHTML= "<img src='alice.jpg'>";
+}
+else if(year === '2006'){
+    document.getElementById("anime").innerHTML= "<img src='ouran.jpg'>";     
+}
+else if(year === '2007'){
+    document.getElementById("anime").innerHTML= "<img src='shugochara.jpg'>";     
+}
+else if(year === '2009'){
+    document.getElementById("anime").innerHTML= "<img src='todoke.jpg'>";
+
+} 
+else if(year === '2010'){
+    document.getElementById("anime").innerHTML= "<img src='maidsama.jpg'>";
+}
+else if(year === '2011'){
+    document.getElementById("anime").innerHTML= "<img src='hunterx.jpg'>";
+}
+else if(year === '2012'){
+    document.getElementById("anime").innerHTML= "<img src='kami.jpg'>";
+}
+else if(year === '2012'){
+    document.getElementById("anime").innerHTML= "<img src='dia.jpg'>";
+}
+else if(year === '2014'){ 
+    document.getElementById("anime").innerHTML= "<img src='haikyuu.jpg'>";  
+
+}
+else if(year === '2015'){
+    document.getElementById("anime").innerHTML= "<img src='aka.webp'>";
+}
+else if(year === '2016'){
+    document.getElementById("anime").innerHTML= "<img src='charlotte.webp'>";     
+}
+}
